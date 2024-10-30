664e253402f4d2d21e781dbe723e3b1a847410fe
VictorCasas-no <victor.casas@enti.cat>
2024-10-30 13:03:40 +0100

    Exercici 6 dels zoos

--- Exercici6.sql
+++ Exercici6.sql
@@ -1,36 +1,34 @@
 CREATE TABLE especies (
-	id INT UNSIGNED AUTO_INCREMENT,
-	nom_vulgar VARCHAR(20),
-	nom_cientific(20),
-	familia ENUM('mamífer','amfibi','au'),
-	en_perill_extincio BOOLEAN,
-	PRIMARY KEY (id)
+    id INT UNSIGNED AUTO_INCREMENT,
+    nom_vulgar VARCHAR(20),
+    nom_cientific VARCHAR(20),
+    familia ENUM('mamífer','amfibi','au'),
+    en_perill_extincio BOOLEAN,
+    PRIMARY KEY (id)
 );
-
 
 CREATE TABLE zoos (
-	id INT UNSIGNED AUTO_INCREMENT,
-	nom VARCHAR(20),
-	ciutat VARCHAR(20),
-	pais ENUM('espanya','frança','marroc','italia')
-	pressupost INT UNSIGNED,
-	grandaria INT UNSIGNED,
-	PRIMARY KEY (id),
-	CHECK (grandaria > 1000 AND grandaria < 1000000)
+    id INT UNSIGNED AUTO_INCREMENT,
+    nom VARCHAR(20),
+    ciutat VARCHAR(20),
+    pais ENUM('espanya','frança','marroc','italia'),
+    pressupost INT UNSIGNED,
+    grandaria INT UNSIGNED,
+    PRIMARY KEY (id),
+    CHECK (grandaria > 1000 AND grandaria < 1000000)
 );
 
-
 CREATE TABLE animals (
-	id INT UNSIGNED AUTO_INCREMENT,
-	num_identificacio INT UNSIGNED UNIQUE,
-	sexe ENUM('mascle','femella'),
-	any_naixement SMALLINT UNSIGNED,
-	pais_origen ENUM('espanya','frança','marroc','italia')
-	continent ENUM('europa','africa')
-	id_especie INT UNSIGNED,
-	id_zoo INT UNSIGNED,
-	FOREIGN KEY (id_especie) REFERENCES especies(id),
-	FOREIGN KEY (id_zoo) REFERENCES zoos(id),
-	PRIMARY KEY (id)
+    id INT UNSIGNED AUTO_INCREMENT,
+    num_identificacio INT UNSIGNED UNIQUE,
+    sexe ENUM('mascle','femella'),
+    any_naixement SMALLINT UNSIGNED,
+    pais_origen ENUM('espanya','frança','marroc','italia'),
+    continent ENUM('europa','africa'),
+    id_especie INT UNSIGNED,
+    id_zoo INT UNSIGNED,
+    FOREIGN KEY (id_especie) REFERENCES especies(id),
+    FOREIGN KEY (id_zoo) REFERENCES zoos(id),
+    PRIMARY KEY (id)
 );
 
